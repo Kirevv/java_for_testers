a17a3a4f0188206b739db76a207ab224745a25f9
kirevv <evgeny.kirillov.mail@gmail.com>
2023-11-15 21:50:08 +0300

    adding group existence check

--- addressbook_web_tests/src/test/java/GroupRemovalTests.java
+++ addressbook_web_tests/src/test/java/GroupRemovalTests.java
@@ -33,6 +33,17 @@ public class GroupRemovalTests {
         if (!isElementPresent(By.name("new"))) {
             driver.findElement(By.linkText("groups")).click();
         }
+        if (!isElementPresent(By.name("selected[]"))) {
+            driver.findElement(By.name("new")).click();
+            driver.findElement(By.name("group_name")).click();
+            driver.findElement(By.name("group_name")).sendKeys("group name");
+            driver.findElement(By.name("group_header")).click();
+            driver.findElement(By.name("group_header")).sendKeys("group header");
+            driver.findElement(By.name("group_footer")).click();
+            driver.findElement(By.name("group_footer")).sendKeys("group footer");
+            driver.findElement(By.name("submit")).click();
+            driver.findElement(By.linkText("group page")).click();
+        }
         driver.findElement(By.linkText("groups")).click();
         driver.findElement(By.name("selected[]")).click();
         driver.findElement(By.name("delete")).click();
