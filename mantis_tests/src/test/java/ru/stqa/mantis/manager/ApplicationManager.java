01567335c4fb81a4d1d8fe1d6899bfa5f35d573c
kirevv <evgeny.kirillov.mail@gmail.com>
2024-01-11 22:23:27 +0300

    mantis user registration with api test

--- mantis_tests/src/test/java/ru/stqa/mantis/manager/ApplicationManager.java
+++ mantis_tests/src/test/java/ru/stqa/mantis/manager/ApplicationManager.java
@@ -15,6 +15,8 @@ public class ApplicationManager {
     private HttpSessionHelper httpSessionHelper;
     private JamesCliHelper jamesCliHelper;
     private MailHelper mailHelper;
+    private JamesApiHelper jamesApiHelper;
+    private RestApiHelper restApiHelper;
 
     public void init(String browser, Properties properties) {
         this.string = browser;
@@ -52,10 +54,6 @@ public class ApplicationManager {
         return httpSessionHelper;
     }
 
-    public String property(String name) {
-        return properties.getProperty(name);
-    }
-
     public JamesCliHelper jamesCli() {
         if (jamesCliHelper == null) {
             jamesCliHelper = new JamesCliHelper(this);
@@ -70,5 +68,22 @@ public class ApplicationManager {
         return mailHelper;
     }
 
+    public JamesApiHelper jamesApi() {
+        if (jamesApiHelper == null) {
+            jamesApiHelper = new JamesApiHelper(this);
+        }
+        return jamesApiHelper;
+    }
+
+    public RestApiHelper rest() {
+        if (restApiHelper == null) {
+            restApiHelper = new RestApiHelper(this);
+        }
+        return restApiHelper;
+    }
+
+    public String property(String name) {
+        return properties.getProperty(name);
+    }
 
 }
--- mantis_tests/src/test/java/ru/stqa/mantis/tests/UserRegistrationTests.java
+++ mantis_tests/src/test/java/ru/stqa/mantis/tests/UserRegistrationTests.java
@@ -3,6 +3,7 @@ package ru.stqa.mantis.tests;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import ru.stqa.mantis.common.CommonFunctions;
+import ru.stqa.mantis.model.UserData;
 
 import java.time.Duration;
 
@@ -18,6 +19,33 @@ public class UserRegistrationTests extends TestBase {
 
         //заполняем форму создания и отправляем (браузер)
         app.session().signup(username, email);
+
+        //ждем почту (MailHelper)
+        var messages = app.mail().receive(email, "password", Duration.ofSeconds(30));
+
+        //извлекаем ссылку из письма
+        var url = CommonFunctions.extractUrl(messages.get(0).content());
+
+        //переходим по ссылке и завершаем регистрацию (браузер)
+        app.session().finishRegistration(url, username, "password");
+
+        //проверяем, что пользователь может залогиниться(HttpSessionHelper)
+        app.session().login(username, "password");
+        Assertions.assertTrue(app.session().isLoggedIn());
+    }
+
+    @Test
+    void canRegisterUserApi() {
+        String username = CommonFunctions.randomString(5);
+        var email = String.format("%s@localhost", username);
+
+        //создание пользователя на почтовом сервере (JamesHelper), используя REST API
+        app.jamesApi().addUser(email, "password");
+
+        //регистрация нового пользователя с помощью REST API
+        app.rest().startRegistration(new UserData()
+                .withUserName(username)
+                .withEmail(email));
 
         //ждем почту (MailHelper)
         var messages = app.mail().receive(email, "password", Duration.ofSeconds(30));
